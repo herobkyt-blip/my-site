481269d97e37e742c1f3d31a0014c408a98521bc
Maxim <herobkyt@gmail.com>
2026-08-20 02:06:09 +0700

    added templates on pages

--- main.go
+++ main.go
@@ -6,34 +6,44 @@ import (
 	"os"
 )
 
+const pageStyle = `
+<style>
+	body {
+		font-family: sans-serif;
+		background-color: #1a1a1a;
+		color: #f0f0f0;
+		max-width: 700px;
+		margin: 40px auto;
+		padding: 0 20px;
+	}
+	a {
+		color: #4ea1f3;
+	}
+</style>
+`
+
+const navBar = `
+<nav>
+	<a href="/">Home</a> | <a href="/projects">Projects</a>
+</nav>
+`
+
 func homePage(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprintf(w, `
 	<html>
 	<head>
 	<title>my first web site</title>
-	<style>
-		body {
-			font-family: sans-serif;
-			background-color: #1a1a1a;
-			color: #f0f0f0;
-			max-width: 700px;
-			margin: 40px auto;
-			padding: 0 20px;
-		}
-		a {
-			color: #4ea1f3;
-			}
-	</style>
+	%s
 	</head>
 	<body>
+	%s
 	<h1>beginner programmer</h1>
 	<p>i dont know what to write here, but i will figure it out</p>
-	<a href="/projects">my project</a>
 	<p><a href="https://t.me/netlydeidlyakadra" target="_blank">my telegramm channel</a></p>
 	</body>
 	</html>
-`)
+`, pageStyle, navBar)
 }
 
 func projectPage(w http.ResponseWriter, r *http.Request) {
@@ -42,18 +52,11 @@ func projectPage(w http.ResponseWriter, r *http.Request) {
 	<html>
 	<head>
 	<title>Projects</title>
-	<style>
-		body {
-			font-family: sans-serif;
-			background-color: #1a1a1a;
-			color: #f0f0f0;
-			max-width: 700px;
-			margin: 40px auto;
-			padding: 0 20px;
-		}
-		a {
-			color: #4ea1f3;
-			}
+	%s
+	</head>
+	<body>
+	%s
+	<h1>it's my projects</h1>
 	</style>
 	</head>
 	<body>
@@ -63,7 +66,7 @@ func projectPage(w http.ResponseWriter, r *http.Request) {
 	<p><a href="https://t.me/netlydeidlyakadra" target="_blank">my telegramm channel</a></p>
 	</body>
 	</html>
-`)
+`, pageStyle, navBar)
 }
 
 func main() {
