620f42ae6936621240a782bae0f72af9f9ace677
Maxim <herobkyt@gmail.com>
2026-08-20 01:37:42 +0700

    updated styles and added projects page

--- main.go
+++ main.go
@@ -10,25 +10,55 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprintf(w, `
 	<html>
-	<head><title>my first web site</title></head>
+	<head>
+	<title>my first web site</title>
+	<style>
+		body {
+			font-family: sans-serif;
+			background-color: #1a1a1a;
+			color: #f0f0f0;
+			max-width: 700px;
+			margin: 40px auto;
+			padding: 0 20px;
+		}
+		a {
+			color: #4ea1f3;
+			}
+	</style>
+	</head>
 	<body>
 	<h1>beginner programmer</h1>
 	<p>i dont know what to write here, but i will figure it out</p>
-	<a href="/about">About Me</a>
+	<a href="/projects">my project</a>
 	<p><a href="https://t.me/netlydeidlyakadra" target="_blank">my telegramm channel</a></p>
 	</body>
 	</html>
 `)
 }
 
-func aboutPage(w http.ResponseWriter, r *http.Request) {
+func projectPage(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprintf(w, `
 	<html>
-	<head><title>About</title></head>
+	<head>
+	<title>Projects</title>
+	<style>
+		body {
+			font-family: sans-serif;
+			background-color: #1a1a1a;
+			color: #f0f0f0;
+			max-width: 700px;
+			margin: 40px auto;
+			padding: 0 20px;
+		}
+		a {
+			color: #4ea1f3;
+			}
+	</style>
+	</head>
 	<body>
-	<h1>About Me</h1>
-	<p>This is a simple about page.</p>
+	<h1>it's my projects</h1>
+	<p><a href="https://t.me/dynastynalbot" target="_blank">Dynasty Bot</a> — Telegram bot on the Go. For Telegram shop.</p>
 	<a href="/">Home</a>
 	<p><a href="https://t.me/netlydeidlyakadra" target="_blank">my telegramm channel</a></p>
 	</body>
@@ -38,7 +68,7 @@ func aboutPage(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", homePage)
-	http.HandleFunc("/about", aboutPage)
+	http.HandleFunc("/projects", projectPage)
 	fmt.Println("Сервер запущен на http://localhost:8080")
 	port := os.Getenv("PORT")
 	if port == "" {
